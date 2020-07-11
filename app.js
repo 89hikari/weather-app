7c9e883358b3d17f879b75f26847476eca0d7aca
89hikari <vladislav0151@bk.ru>
2020-07-11 22:16:31 +0300

    some css + js with weather's API

---
+++ app.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let tempDescription = document.querySelector('.temperature-description');
+    let tempDegree = document.querySelector('.temperature-degree');
+    let location = document.querySelector('.location-timezone');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ebbba3c34b305988df247c86684ecc0f`;
+            fetch(api).then(response => {
+                return response.json();
+            }).then(data => {
+                console.log(data);
+                tempDegree.textContent = (data.main.temp - 273,15).toFixed(1);
+                tempDescription.textContent = data.weather[0].description;
+            });
+        });
+    } else {
+        h1.textContent = "To start this application you need enable GPS location in your browser. Please google how."
+    }
+
+});
